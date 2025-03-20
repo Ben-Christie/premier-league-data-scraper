24707488af1b8c07f848f649fc55393da47e5696
unknown <ben.christie11@gmail.com>
2025-03-20 20:19:15 +0000

    added new nations represented in premier league

--- data.py
+++ data.py
@@ -5,18 +5,18 @@ urls = [
     "https://fbref.com/en/squads/4ba7cbea/Bournemouth-Stats",
     "https://fbref.com/en/squads/cd051869/Brentford-Stats",
     "https://fbref.com/en/squads/d07537b9/Brighton-and-Hove-Albion-Stats",
-    "https://fbref.com/en/squads/943e8050/Burnley-Stats",
     "https://fbref.com/en/squads/cff3d9bb/Chelsea-Stats",
     "https://fbref.com/en/squads/47c64c55/Crystal-Palace-Stats",
     "https://fbref.com/en/squads/d3fd31cc/Everton-Stats",
     "https://fbref.com/en/squads/fd962109/Fulham-Stats",
+    "https://fbref.com/en/squads/b74092de/Ipswich-Town-Stats",
+    "https://fbref.com/en/squads/a2d435b3/Leicester-City-Stats",
     "https://fbref.com/en/squads/822bd0ba/Liverpool-Stats",
-    "https://fbref.com/en/squads/e297cd13/Luton-Town-Stats",
     "https://fbref.com/en/squads/b8fd03ef/Manchester-City-Stats",
     "https://fbref.com/en/squads/19538871/Manchester-United-Stats",
     "https://fbref.com/en/squads/b2b47a98/Newcastle-United-Stats",
     "https://fbref.com/en/squads/e4a775cb/Nottingham-Forest-Stats",
-    "https://fbref.com/en/squads/1df6b87e/Sheffield-United-Stats",
+    "https://fbref.com/en/squads/33c895d4/Southampton-Stats",
     "https://fbref.com/en/squads/361ca564/Tottenham-Hotspur-Stats",
     "https://fbref.com/en/squads/7c21e445/West-Ham-United-Stats",
     "https://fbref.com/en/squads/8cec06e1/Wolverhampton-Wanderers-Stats"
@@ -45,6 +45,7 @@ nations = {
     'BFA': 'Burkina Faso',
     'BIH': 'Bosnia and Herzegovina',
     'BRA': 'Brazil',
+    'BRB': 'Barbados',
     'CAN': 'Canada',
     'CIV': 'Ivory Coast',
     'CHI': 'Chile',
@@ -63,6 +64,7 @@ nations = {
     'FIN': 'Finland',
     'FRA': 'France',
     'GAB': 'Gabon',
+    'GAM': 'Gambia',
     'GER': 'Germany',
     'GHA': 'Ghana',
     'GNB': 'Guinea-Bissau',
@@ -74,6 +76,7 @@ nations = {
     'ISL': 'Iceland',
     'ISR': 'Israel',
     'IRN': 'Iran',
+    'IRQ': 'Iraq',
     'ITA': 'Italy',
     'JAM': 'Jamaica',
     'JPN': 'Japan',
@@ -82,6 +85,7 @@ nations = {
     'MAR': 'Morocco',
     'MEX': 'Mexico',
     'MLI': 'Mali',
+    'MSR': 'Montserrat',
     'NED': 'Netherlands',
     'NIR': 'Northern Ireland',
     'NGA': 'Nigeria',
@@ -104,11 +108,8 @@ nations = {
     'UKR': 'Ukraine',
     'URU': 'Uruguay',
     'USA': 'United States of America',
+    'UZB': 'Uzbekistan',
     'WAL': 'Wales',
+    'ZAM': 'Zambia',
     'ZIM': 'Zimbabwe'
 }
-
-# exclude data points containing these titles
-exclude_keywords = [
-    "90", "pct", "gca", "per", "x", "sca", "plus", "matches", "games_complete", "average_shot_distance", "goals_assists", "goals_pens", 'tackles_interceptions', 'cards_yellow_red', 'gk_games', 'gk_games_starts', 'gk_minutes', 'gk_goals_against', 'gk_wins', 'gk_ties', 'gk_losses', 'gk_avg_distance_def_actions'
-]
