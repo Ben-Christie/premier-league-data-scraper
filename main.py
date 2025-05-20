44372c8bef5a7025f0e72e19ea01ae0c3226c2e7
unknown <ben.christie11@gmail.com>
2025-05-20 22:22:51 +0100

    implemented sofifa scraper and created new file for transfermarkt

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from helper import csv_split
 from fbref_scraper import fbref
 from sofifa_scraper import sofifa
-from transfermarkt_scraper import transfermarkt
 
 # init web driver
 driver = webdriver.Chrome()
@@ -15,7 +13,6 @@ players = {}
 # fbref scraper is responsible for gathering initial dataset of players, without it running no scraper will be able to append data
 fbref(players, driver)
 sofifa(players, driver)
-transfermarkt(players, driver)
 
 # Close the webdriver instance
 driver.quit()
--- transfermarkt_scraper.py
+++
@@ -1,2 +0,0 @@
-def transfermarkt(players, driver):
-    print("transfermarkt")
